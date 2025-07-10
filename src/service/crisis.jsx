081c3c78ad3b154a2bca7d54c0b16fc92e61713c
guoxuta <152752227+guoxuta@users.noreply.github.com>
2025-07-10 10:47:04 +0800

    front_new

--- src/service/crisis.jsx
+++ src/service/crisis.jsx
@@ -1,5 +1,48 @@
 import { PREFIX, getJson, post, put, del } from "./common";
 
+
+export async function getAuditingCrisis() {
+    const url = `${PREFIX}/crisis/listAuditing`;
+    let res;
+    try {
+        res = await getJson(url);
+    } catch (e) {
+        console.log(e);
+        res = [];
+    }
+    return res;
+}
+
+export async function getAllConfirmedCrisis() {
+    const url = `${PREFIX}/crisis/getall`;
+    let res;
+    try {
+        res = await getJson(url);
+    } catch (e) {
+        console.log(e);
+        res = [];
+    }
+    return res;
+}
+
+export async function confirmCrisis(crisisId,urgencyLevel) {
+    const url = `${PREFIX}/crisis/confirm`;
+    try {
+        await post(url, { crisisid:crisisId , urgencylevel:urgencyLevel });
+    } catch (e) {
+        throw e;
+    }
+}
+
+export async function deleteCrisis(crisisId) {
+    const url = `${PREFIX}/crisis/delete`;
+    try {
+        await post(url, { crisisid:crisisId });
+    } catch (e) {
+        throw e;
+    }
+}
+
 export async function getCrisis() {
     const url = `${PREFIX}/crisis/getall`;
     let res;
